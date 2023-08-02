517e5ee36b63b8a7db6492e95284c238e10df758
Pyry Kontio <pyry.kontio@drasa.eu>
2023-08-03 03:39:52 +0900

    It goes to config

--- make_test_env.sh
+++ make_test_env.sh
@@ -3,11 +3,15 @@
 nix build .
 mkdir -p test_env
 nix copy --to ./test_env $(realpath result) --no-check-sigs
-mkdir -p test_env/bin test_env/dev test_env/etc/config
+mkdir -p test_env/bin test_env/proc/net test_env/dev test_env/etc/config
 test -d source || git clone https://git.openwrt.org/project/odhcpd.git source
 nix develop --command cmake -S source -B source/build
-nix develop --command cmake --build source/build
+nix develop --command cmake --build source/build -v
 cp ./source/build/odhcpd ./test_env/bin/odhcpd
+sudo mknod ./test_env/dev/urandom c 1 9
+cat > ./test_env/proc/net/ipv6_route <<EOF
+00000000000000000000000000000000 00 00000000000000000000000000000000 00 00000000000000000000000000000000 00000100 0000000b 00000000 00000001     eno1
+EOF
 
 sudo chroot ./test_env /bin/odhcpd -l7
 
